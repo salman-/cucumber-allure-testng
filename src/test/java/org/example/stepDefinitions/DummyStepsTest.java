bfa6527ad6beab1fc0588bdc73d423bcf9bd7288
lashksal <salman.lashkarara@diconium.com>
2023-12-15 21:09:20 +0100

    remove unused tag

--- src/test/java/org/example/stepDefinitions/DummyStepsTest.java
+++ src/test/java/org/example/stepDefinitions/DummyStepsTest.java
@@ -4,10 +4,8 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.qameta.allure.Step;
-import org.testng.annotations.Test;
 
-@Test
-public class DummySteps {
+public class DummyStepsTest {
 
 
   @Step("the user is on the login page")
@@ -15,16 +13,19 @@ public class DummySteps {
   public void the_user_is_on_the_login_page() {
 
   }
+
   @Step("the user enters valid credentials")
   @When("the user enters valid credentials")
   public void the_user_enters_valid_credentials() {
 
   }
+
   @Step("clicks the login button")
   @When("clicks the login button")
   public void clicks_the_login_button() {
 
   }
+
   @Step("the user should be redirected to the dashboard")
   @Then("the user should be redirected to the dashboard")
   public void the_user_should_be_redirected_to_the_dashboard() {
