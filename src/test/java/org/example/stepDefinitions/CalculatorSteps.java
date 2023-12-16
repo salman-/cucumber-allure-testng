5cf774e81db62cdcc6eb1d5c045ae8e50ddca1f4
lashksal <salman.lashkarara@diconium.com>
2023-12-16 12:00:51 +0100

    remove Allure steps

--- src/test/java/org/example/stepDefinitions/CalculatorSteps.java
+++ src/test/java/org/example/stepDefinitions/CalculatorSteps.java
@@ -3,34 +3,29 @@ package org.example.stepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.qameta.allure.Step;
 
 public class CalculatorSteps {
 
-  @Step("the calculator is turned on")
   @Given("the calculator is turned on")
   public void the_calculator_is_turned_on() {
 
   }
 
-  @Step("I enter the number {int}")
   @When("I enter the number {int}")
   public void i_enter_the_number(Integer int1) {
 
   }
 
-  @Step("I press the addition button")
   @When("I press the addition button")
   public void i_press_the_addition_button() {
 
   }
 
-  @Step("I press the equals button")
   @When("I press the equals button")
   public void i_press_the_equals_button() {
 
   }
-  @Step("the result should be {int} on the screen")
+
   @Then("the result should be {int} on the screen")
   public void the_result_should_be_on_the_screen(Integer int1) {
 
--- src/test/java/org/example/stepDefinitions/DummyStepsTest.java
+++ src/test/java/org/example/stepDefinitions/DummyStepsTest.java
@@ -3,36 +3,30 @@ package org.example.stepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.qameta.allure.Step;
 
 public class DummyStepsTest {
 
 
-  @Step("the user is on the login page")
   @Given("the user is on the login page")
   public void the_user_is_on_the_login_page() {
 
   }
 
-  @Step("the user enters valid credentials")
   @When("the user enters valid credentials")
   public void the_user_enters_valid_credentials() {
 
   }
 
-  @Step("clicks the login button")
   @When("clicks the login button")
   public void clicks_the_login_button() {
 
   }
 
-  @Step("the user should be redirected to the dashboard")
   @Then("the user should be redirected to the dashboard")
   public void the_user_should_be_redirected_to_the_dashboard() {
 
   }
 
-  @Step("should see a welcome message")
   @Then("should see a welcome message")
   public void should_see_a_welcome_message() {
 
